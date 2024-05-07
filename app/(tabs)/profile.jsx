2af5312d0d74e95ee58e50660ee89b14d1f09795
idsta.ivandi <idsta.ivandi@xl.co.id>
2024-05-07 23:15:16 +0700

    setting up folder structure

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
@@ -10,5 +10,3 @@ const Profile = () => {
 }
 
 export default Profile
-
-const styles = StyleSheet.create({})
