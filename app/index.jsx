3161fb4a2a333ec505a2da16b42854f3c569c325
idsta.ivandi <idsta.ivandi@xl.co.id>
2024-05-07 22:29:28 +0700

    init

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hahahahah!</Text>
       <StatusBar style="auto" />
+      <Link 
+        href='/profile'
+        style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
   );
 }
